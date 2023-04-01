cb6b87a47c9c6ac75ce5cd8c88a0aaabe5562558
Yisus <yisusxcraist@gmail.com>
2023-04-01 01:32:06 -0600

    readme

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# Geometria_AV
-Repositorio OS de funciones y ejemplos de Geometria Analitica Vectorial
+# Analytic Vectorial Geometry
+
+This is an open-source Python library for performing operations with vectors in 3-dimensional space. The library provides functions for defining vectors, performing arithmetic operations with vectors (addition, subtraction, scalar multiplication), and computing the dot product and cross product of two vectors.
+
+## Installation
+
+To use this library, you can clone the repository from GitHub:
+
+```bash
+git clone https://github.com/username/analytic-vectorial-geometry.git
+
+Usage
+To use the library in your own Python code, you can import the Vector module:
+
+from Vector import Vector
+
+v1 = Vector(1, 2, 3)
+v2 = Vector(4, 5, 6)
+
+# Add two vectors
+v3 = v1 + v2
+
+# Subtract two vectors
+v4 = v1 - v2
+
+# Multiply a vector by a scalar
+v5 = v1 * 2
+
+# Compute the dot product of two vectors
+dot_product = v1.dot(v2)
+
+# Compute the cross product of two vectors
+v6 = v1.cross(v2)
+
+Testing
+This repository also includes a test suite for the Vector module. To run the tests, you can use the following command:
+
+python Tests.py
+
+The tests include checks for addition, subtraction, scalar multiplication, dot product, and cross product operations. The test suite also includes 3D plots of the vectors, which can be used to visually verify the results of the operations.
+
+Contributing
+Contributions to this library are welcome. To contribute, please fork the repository and submit a pull request with your changes. Be sure to include tests for any new functionality you add.
+
+License
+This library is licensed under the MIT License. See the LICENSE file for details.
--- Tests.py
+++ Tests.py
@@ -1,30 +1,50 @@
 import unittest
 from Vector import Vector
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class TestVector(unittest.TestCase):
     def setUp(self):
-        self.v1 = Vector([1, 2, 3])
-        self.v2 = Vector([4, 5, 6])
-    
+        self.v1 = Vector(1, 2, 3)
+        self.v2 = Vector(4, 5, 6)
+
     def test_add(self):
-        result = self.v1 + self.v2
-        self.assertEqual(result, Vector([5, 7, 9]))
-        
+        v = self.v1 + self.v2
+        self.assertEqual(v.x, 5)
+        self.assertEqual(v.y, 7)
+        self.assertEqual(v.z, 9)
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.set_xlim(0, 10)
+        ax.set_ylim(0, 10)
+        ax.set_zlim(0, 10)
+        ax.quiver(0, 0, 0, self.v1.x, self.v1.y, self.v1.z, color='red')
+        ax.quiver(self.v1.x, self.v1.y, self.v1.z, self.v2.x, self.v2.y, self.v2.z, color='blue')
+        ax.quiver(0, 0, 0, v.x, v.y, v.z, color='green')
+        plt.show()
+
     def test_subtract(self):
-        result = self.v1 - self.v2
-        self.assertEqual(result, Vector([-3, -3, -3]))
-        
+        v = self.v1 - self.v2
+        self.assertEqual(v.x, -3)
+        self.assertEqual(v.y, -3)
+        self.assertEqual(v.z, -3)
+
     def test_scalar_multiply(self):
-        result = self.v1 * 2
-        self.assertEqual(result, Vector([2, 4, 6]))
-        
+        v = self.v1 * 2
+        self.assertEqual(v.x, 2)
+        self.assertEqual(v.y, 4)
+        self.assertEqual(v.z, 6)
+
     def test_dot_product(self):
         result = self.v1.dot(self.v2)
         self.assertEqual(result, 32)
-        
+
     def test_cross_product(self):
-        result = self.v1.cross(self.v2)
-        self.assertEqual(result, Vector([-3, 6, -3]))
+        v = self.v1.cross(self.v2)
+        self.assertEqual(v.x, -3)
+        self.assertEqual(v.y, 6)
+        self.assertEqual(v.z, -3)
         
+    
 if __name__ == '__main__':
     unittest.main()
